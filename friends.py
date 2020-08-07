7bcbd52b1c7238f02e2ba9e0dad8484dec97601a
sgwooo <52553083+sgwooo@users.noreply.github.com>
2020-08-07 09:52:36 +0900

    미드 대본으로 영어 공부 자료 만들기

--- friends.py
+++ friends.py
@@ -1,5 +1,5 @@
-# 드라마 텍스트 가공  (Do it 파이썬 생활프로그래밍) 초판 2020: 52페이지
-# friends.txt를 다운받아서 같은 폴더에 넣은 다음 프로그램을 실행합니다. 
+# 드라마 텍스트 가공  (Do it 파이썬 생활프로그래밍) 초판 2020: 99페이지
+# friends101.txt를 다운받아서 같은 폴더에 넣은 다음 프로그램을 실행합니다. 
 import os, re, codecs
 #자신의 저장경로를 입력해야 합니다. 
 os.chdir(r' ')
@@ -19,7 +19,7 @@ Line = re.findall(r'Monica:.+', script101)
 f = open('monica.txt', 'w', encoding = 'utf-8')
 monica = ''
 for i in Line:
-	monica += i +'\n'
+    monica += i +'\n'
 f.write(monica)
 # 파일 닫기 
 f.close()	
@@ -29,35 +29,36 @@ for item in Line[:3]:
     print(item)
 
 # 등장인물 이름 모으기 
-print(re.findall(r'[A-Z][a-z]+: ', script101) )
+print(re.findall(r'[A-Z][a-z]+: ', script101))
 
 # 중복 지우기
 print(set(re.findall(r'[A-Z][a-z]+: ', script101)))
 
 # 캐릭터 이름 한 줄로 출력하기
-character = [x[:-2] for x in list(set(re.findall(r'[A-Z][a-z]+: ',script101)))]
+character = [x[:-2] for x in list(set(re.findall(r'[A-Z][a-z]+: ', script101)))]
 
 # 캐릭터 이름 각각 출력하기 
 for i in character:
     print(i)
 
 # 지문만 출력하기
-re.findall(r'\([A-Za-z].+[a-z|\.]\)', script101, re.VERBOSE) [:6]
-f = open('friends101.txt','r')
+re.findall(r'\([A-Za-z].+[a-z|\.]\)', script101, re.VERBOSE)[:6]
+f = open('friends101.txt', 'r')
 sentences = f.readlines()
 # 20개만 출력해보기
 for i in sentences[:20]:			## 먼저 문장 20개만 가져와 실험해 보겠습니다
-    if re.match(r'[A-Z][a-z]+:', i): 	## match 문으로 문장 맨 앞에서 패턴을 찾습니다 
+    if re.match(r'[A-Z][a-z]+:', i):		## match 문으로 문장 맨 앞에서 패턴을 찾습니다 
         print(i)
 
 # would가 나오는 문장만 추출하기 
-would = [ i for i in sentences if re.match(r'[A-Z][a-z]+:', i) and re.search('would', i)]
+would = [i for i in sentences if re.match(r'[A-Z][a-z]+:', i) and re.search('would', i)]
 # take가 들어간 문장만 추출하기 
-take = [ i for i in sentences if re.match(r'[A-Z][a-z]+:', i) and re.search(' take',i)]
+take = [i for i in sentences if re.match(r'[A-Z][a-z]+:', i) and re.search(' take', i)]
 # take가 들어간 문장 출력하기
 for i in take:
-	print(i)
+    print(i)
  
 # would가 들어간 문장 파일로 만들기 
-newf = open('would.txt','w')	
+newf = open('would.txt', 'w')	
 newf.writelines(would)		
+newf.close()
