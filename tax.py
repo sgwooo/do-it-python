adf2f987366bd66f78f59385122e354ef4cd6b08
sgwooo <52553083+sgwooo@users.noreply.github.com>
2020-08-07 09:28:08 +0900

    Update tax.py

--- tax.py
+++ tax.py
@@ -2,9 +2,9 @@
 def service_price():
     service = input('서비스 종류를 입력하세요, a/b/c: ')	## 서비스 종류를 입력
     valueAdded = input('부가 가치세를 포함합니까? y/n: ')	## 부가세 포함 여부 입력
-    if valueAdded == 'y':					## 부가세가 있을 때
-        if service == 'a': 				        ## 서비스 종류가 a라면
-            result = 23 * 1.1 				    ## 23만 원에 1.1을 곱하여 result에 저장
+    if valueAdded == 'y':                       ## 부가세가 있을 때
+        if service == 'a':                      ## 서비스 종류가 a라면
+            result = 23 * 1.1                   ## 23만 원에 1.1을 곱하여 result에 저장
         if service == 'b':
             result = 40 * 1.1
         if service == 'c':
